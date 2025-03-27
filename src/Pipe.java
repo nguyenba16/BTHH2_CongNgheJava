31e7ceb624f1de846df5a7c7b705060b99062cdc
nguyenba16 <congba2004@gmail.com>
2025-03-27 20:57:38 +0700

    final version

--- src/Pipe.java
+++ src/Pipe.java
@@ -4,8 +4,21 @@ import java.awt.*;
 public class Pipe extends JPanel {
     int x;
     int y;
+    int width = 50;
+    int height;
     Image img;
-    public Pipe(Image img) {
+    int speed = 2;
+    boolean passed = false;
+
+    public Pipe(int x, int y, int height, Image img) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
         this.img = img;
     }
+
+    public void update() {
+        x -= speed;
+    }
+
 }
